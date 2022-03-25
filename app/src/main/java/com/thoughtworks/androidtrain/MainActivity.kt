d7e4ba9dffb6630fa5425d4d018f6b728e9c67f0
pingping2116 <ping.ping@thoughtworks.com>
2022-03-25 15:14:21 +0800

    feat(02-2): add frame layout homework

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.thoughtworks.androidtrain
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val linear = findViewById<View>(R.id.linear_layout) as LinearLayout
+
+        for(index in 1..10){
+            var button_temp=Button(ContextThemeWrapper(this, R.style.scroll_button_group))
+            button_temp.setText("button_$index")
+            linear.addView(button_temp)
+        }
+
     }
 }
